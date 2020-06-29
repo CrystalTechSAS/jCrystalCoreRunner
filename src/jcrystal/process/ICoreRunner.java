ce75a12189a3faae10e7d8f1944edf5e348f98d5
gasotelo <gasotelo@gmail.com>
2020-06-29 16:56:01 -0500

    Update ICoreRunner.java

--- src/jcrystal/process/ICoreRunner.java
+++ src/jcrystal/process/ICoreRunner.java
@@ -7,5 +7,5 @@ import jcrystal.preprocess.main.MainPreprocessor;
 
 public interface ICoreRunner {
 	void setup(MainPreprocessor preprocessor, TreeSet<String> CHECKED_CLASSES);
-	boolean dojCrystalProcess(JCrystalMetaConfig config)throws Exception;
+	void dojCrystalProcess(JCrystalMetaConfig config)throws Exception;
 }
